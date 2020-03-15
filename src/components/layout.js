e08fc7f7b414728c0d563c180bdbfef3a36cf5bc
Jeremy Mullis <jeremy.mullis@gmail.com>
2020-03-15 14:19:36 -0500

    add back-to-top thing

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,12 +9,18 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+import { addBackToTop } from 'vanilla-back-to-top'
+
 import Header from "./header"
 import Navigation from "./navigation"
 import ContactForm from "./contact-form"
 import Footer from "./footer"
 
 import "../styles/main.css"
+
+addBackToTop({
+  backgroundColor: '#4F3590',
+})
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
