d1309683f46a69ea0284849d7042043b1aa1fa72
Jeremy Mullis <jeremy.mullis@gmail.com>
2020-03-15 16:20:50 -0500

    enable smooth scrolling in CSS

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,8 +3,9 @@ import React from "react"
 const navigation = () => (
   <nav className="fixed top-0 w-full z-20 text-right">
     <a
+      id="contactButton"
       href="#contact"
-      className="inline-block bg-purple text-white p-4 uppercase mr-4 hover:bg-orange"
+      className="inline-block bg-purple text-white p-4 uppercase mr-4 hover:bg-orange shadow-md"
     >
       Contact
     </a>
