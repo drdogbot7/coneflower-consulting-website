310e198636ff5e50e0908a3b19a447fbff54bbd8
Jeremy Mullis <jeremy.mullis@gmail.com>
2020-11-02 16:49:13 -0600

    add instagram to nav

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebook,
+  faInstagram,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 const navigation = () => (
   <nav className="fixed top-0 z-20 w-full">
@@ -11,6 +15,14 @@ const navigation = () => (
           className="block px-2 py-1 mr-4 text-white uppercase shadow-md bg-purple hover:bg-orange"
         >
           <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+      </li>
+      <li className="">
+        <a
+          className="block px-2 py-1 mr-4 text-white uppercase shadow-md bg-purple hover:bg-orange"
+          href="https://www.instagram.com/coneflowerconsulting/"
+        >
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
         </a>
       </li>
       <li className="">
